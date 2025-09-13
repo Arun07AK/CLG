1ceb789b70dc030f0ef2aab419156daea9a541bd
ARUN AK <arunarjunan9083@gmail.com>
2025-09-13 12:27:12 +0530

    Create program.cpp

---
+++ DSA/LINKED LIST/CIRCULAR/Deletion in Circular Linked List/program.cpp
@@ -0,0 +1,54 @@
+/*
+class Node {
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* deleteNode(Node* head, int key) {
+        // code here
+        if(head==nullptr)
+        {
+            return head;
+        }
+        if(head->data==key)
+        {
+            Node* p=head;
+            while(p->next!=head)
+            {
+                p=p->next;
+            }
+            p->next=head->next;
+            Node* temp=head;
+            head=head->next;
+            delete temp;
+            return head;
+            
+        }
+        else
+        {
+            Node* p=head;
+            while(p->next!=head )
+            {
+                if(p->next->data==key)
+                {
+                    Node* temp;
+                    temp=p->next;
+                    p->next=p->next->next;
+                    delete temp;
+                    return head;
+                }
+                p=p->next;
+                
+            }
+            return head;
+        }
+        
+    }
+};
